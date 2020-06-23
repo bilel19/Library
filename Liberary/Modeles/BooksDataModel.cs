e3054bf1ba85900affad69189ecde7e9491eba37
bilel <bilokas2017@gmail.com>
2020-06-23 22:20:25 +0100

    first commit

---
+++ Liberary/Modeles/BooksDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liberary.Modeles
+{
+    class BooksDataModel
+    {
+        public int Book_ID { get;set; }
+        public int Book_Name { get; set; }
+        public int Book_Aythor { get; set; }
+        public int Book_PageNumber { get; set; }
+
+    }
+}
